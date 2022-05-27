0917c49b4dc4d3b06c784bdd212c0e75f6418190
İlker Gündüz <ilkergndz3500@gmail.com>
2022-05-28 02:36:55 +0300

    Migration

---
+++ database/migrations/2022_05_27_232734_info.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Info extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('info',function (Blueprint $table){
+            $table->increments('id');
+            $table->integer('userid')->default(0);
+            $table->string('job')->nullable();
+            $table->string('school')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('info');
+    }
+}
