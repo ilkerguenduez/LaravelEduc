f177c49e1620291dac502e0f7763a14eb40e87ac
İlker Gündüz <ilkergndz3500@gmail.com>
2022-05-28 02:48:07 +0300

    Migration ve seed

---
+++ database/seeders/InfoSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class InfoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('info')->insert([
+            'userid'=>1,
+            'job'=>'Software Engineer',
+            'school'=>'Fırat University',
+            'created_at'=>date("Y-m-d H:i:s"),
+            'updated_at'=>date("Y-m-d H:i:s")
+        ]);
+    }
+}
